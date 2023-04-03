a0fd04f447daf6e7d597f39a79848c72f184e825
EngAmrs <amr.abdoullah@gmail.com>
2023-04-03 15:37:57 +0200

    Amend files

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,8 @@
 import express, { Express } from 'express';
 import * as dotenv from 'dotenv';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import mongoose from 'mongoose';
 
+// Read the content inside dotenv
 dotenv.config();
 
 // Connect to mongoose
@@ -10,8 +10,6 @@ const mongoUrl : string = process.env.MONGO_URL as string;
 mongoose.connect(mongoUrl)
   .then(() => console.log('Database is connected'))
   .catch(() => console.log('Database connection failed'));
-
-// Read the content inside dotenv
 
 // creating express server
 const app: Express = express();
