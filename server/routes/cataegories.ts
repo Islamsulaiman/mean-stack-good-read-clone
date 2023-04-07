5eb0fcab021bc428cea8998008989181bfcec063
moustafa mahmoud <mostafa09mahmoud@gmail.com>
2023-04-07 08:00:04 +0200

    added validation Category Routes

--- server/routes/cataegories.ts
+++ server/routes/cataegories.ts
@@ -4,17 +4,28 @@ import {
 } from '../middlewares/cataegories';
 
 import { errorHandling } from '../middlewares/errorHandling';
+import * as validation from '../middlewares/validateInput';
 
 const router = Router();
 
 // 1.create Category
-router.post('/', errorHandling(createCategory));
+router.post(
+  '/',
+  validation.checkCatagoryName,
+  validation.validateInput,
+  errorHandling(createCategory),
+);
 // 2.get all categories
 router.get('/', getAllCategories);
 // 3.get one category
-router.get('/:id', getOneCategory);
+router.get('/:id', errorHandling(getOneCategory));
 // 4.update one category
-router.patch('/:id', errorHandling(updateCategory));
+router.patch(
+  '/:id',
+  validation.checkCatagoryName,
+  validation.validateInput,
+  errorHandling(updateCategory),
+);
 // 5.delete  category
 router.delete('/:id', errorHandling(deleteCategory));
 export const categoryRoute : Router = router;
