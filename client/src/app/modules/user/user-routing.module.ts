42c67a9e9b117ce3e0b1ea6ba62660509ab68bf1
Islam Sulaiman <ma.eslam@yahoo.com>
2023-04-10 14:52:44 +0200

    create register component with auth service

--- client/src/app/modules/user/user-routing.module.ts
+++ client/src/app/modules/user/user-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 const routes: Routes = [
 
-  { path: '', component: UserComponent }
+  { path: '', component: UserComponent },
+  { path: 'register', component: RegistrationComponent },
+
   
 ];
 
--- client/src/app/modules/user/user.module.ts
+++ client/src/app/modules/user/user.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
+import { AuthService } from './services/auth.service';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 
 @NgModule({
   declarations: [
-    UserComponent
+    UserComponent,
+    AuthService,
+    RegistrationComponent,
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule, 
   ]
 })
 export class UserModule { }
