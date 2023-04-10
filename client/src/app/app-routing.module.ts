87ca28381dddb70668f260d9fe61234bf03cb77b
Amr Salah <amr.abdoullah@gmail.com>
2023-04-10 02:39:22 +0200

    Client modules are created [User & Admin]

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) },
+  { path: '', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
