e0ee06e99f730dbd41977993e7e2d2fe0febeefe
moustafa mahmoud <mostafa09mahmoud@gmail.com>
2023-04-11 20:14:09 +0200

    Authors Service

--- client/src/app/modules/admin/services/authors.service.ts
+++ client/src/app/modules/admin/services/authors.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorsService {
 
-  constructor() { }
+
+  URL = 'http://localhost:3000/category';
+  constructor(private _HttpClient:HttpClient) { }
+
+  //Add Author
+  addAuthor(data:any):Observable<any>  {
+    return this._HttpClient.post(this.URL, data);
+  }
+
+  //get Authors
+  getAuthors():Observable<any> {
+    return this._HttpClient.get(this.URL);
+  }
+
+  //get Author By Id
+  getAuthorById(id:number):Observable<any> {
+    return this._HttpClient.get(`${this.URL}/${id}`);
+  }
+
+  //update Author
+  updateAuthor(id:number,data:any):Observable<any>{
+    return this._HttpClient.put(`${this.URL}/${id}`, data);
+  }
+
+  //delete Author
+  deleteAuthor(id:number):Observable<any> {
+    return this._HttpClient.delete(`${this.URL}/${id}`);
+  }
 }
