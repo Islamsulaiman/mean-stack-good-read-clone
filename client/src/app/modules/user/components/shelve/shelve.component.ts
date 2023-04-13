0728133a885bfa8ede8d3824017df73e223525c4
Islam Sulaiman <ma.eslam@yahoo.com>
2023-04-13 23:54:17 +0200

    Design: client => starter design for the user book shelve

--- client/src/app/modules/user/components/shelve/shelve.component.ts
+++ client/src/app/modules/user/components/shelve/shelve.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shelve',
   templateUrl: './shelve.component.html',
   styleUrls: ['./shelve.component.css']
 })
-export class ShelveComponent {
+export class ShelveComponent   {
 
+
+  selectStar(index: number): number {
+    const stars = document.querySelectorAll('.rating .star');
+    for (let i = 0; i < stars.length; i++) {
+      if (i < index) {
+        stars[i].classList.add('selected');
+      } else {
+        stars[i].classList.remove('selected');
+      }
+    }
+    console.log(index)
+    return index
+}
 }
