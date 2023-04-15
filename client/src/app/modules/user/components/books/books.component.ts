ee65984b476919807bef3ea9e939e6f02e0df6a0
Islam Sulaiman <ma.eslam@yahoo.com>
2023-04-15 14:23:25 +0200

    Functionality: client => add pagination for books component

--- client/src/app/modules/user/components/books/books.component.ts
+++ client/src/app/modules/user/components/books/books.component.ts
@@ -10,22 +10,52 @@ export class BooksComponent {
 
   skip = 0
   limit = 12
+  currentPage = 1
+  totalPages = 0
 
   books:any[] = []
   error  ="";
 
-  constructor(private _BooksService: BooksService){
-
-    //assume that every page have only 10 books, th 1st page from 0 to 10
+  loadCategories() {
     this._BooksService.getAllBooks(this.skip, this.limit, {observe: 'response'}).subscribe((res)=>{
       
+      console.log(res)
       if(res.status === 200){
-        this.books = res.body.book
+        this.books = res.body.book.books
+        this.totalPages = res.body.book.noOfPages
       }else{
         this.error = res
       }
-
     })
   }
+
+  constructor(private _BooksService: BooksService){
+    this.loadCategories();
+  }
+
+
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.skip = this.skip - this.limit;
+      console.log("prev")
+      this.loadCategories();
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      console.log("nexrt")
+      this.currentPage++;
+      this.skip = this.skip + this.limit
+      this.loadCategories();
+    }
+  }
+
+
+
+
+
 
 }
