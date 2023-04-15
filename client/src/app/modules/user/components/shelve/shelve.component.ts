4aa0b94b5011c8cdeb56ca140cbf4263bd6d12d6
Islam Sulaiman <ma.eslam@yahoo.com>
2023-04-15 14:58:45 +0200

    Functionality: client => make book shelve comp rerender when book rating change to view the new rating

--- client/src/app/modules/user/components/shelve/shelve.component.ts
+++ client/src/app/modules/user/components/shelve/shelve.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ChangeDetectorRef, Renderer2 } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 import { BehaviorSubject } from 'rxjs';
 
@@ -35,7 +35,7 @@ export class ShelveComponent implements OnInit {
   currentPage = 1
   totalPages = 0
 
-  constructor(private _UserService:UsersService){
+  constructor(private _UserService:UsersService, private _cdr:ChangeDetectorRef, private _renderer: Renderer2){
     this.loadData()
     // this._UserService.getUserById("643890010923d775b0ea7872", this.skip, this.limit,{observe: 'response'}).subscribe((res)=>{
 
@@ -132,53 +132,23 @@ export class ShelveComponent implements OnInit {
 
     this._UserService.changeBookRating(book.bookId._id, this.userId, index,{observe: 'response'}).subscribe((res)=>{
       console.log(res)
+
     })
 
-    window.location.reload();
+    // this block of code is to remove rating dive and re-inserting it again to trigger detectChanges() to rebder the comp
+    book.rating=index
+    const userRatingElement = document.querySelector(`#book-${cardNumber} .userRating`);
+    this._renderer.removeChild(userRatingElement?.parentNode, userRatingElement);
+    const newRatingElement = this._renderer.createElement('div');
+    const ratingText = this._renderer.createText(`${book.rating}x`);
+    this._renderer.appendChild(newRatingElement, ratingText);
+    this._renderer.appendChild(document.querySelector(`#book-${cardNumber} .bookDetails`), newRatingElement);
+
+
+    this._cdr.detectChanges()
+    
     return index
   }
-
-
-  // selectStar(index: number, cardNumber: number): number {
-
-  //   const stars = document.querySelectorAll('.rating .star'); //array of all stars
-
-  //   console.log("stars",stars)
-  //   console.log("card number", cardNumber)
-  //   console.log("stars length", stars.length)
-
-  //   if(cardNumber === 1) {
-  //     for (let i = 0; i < stars.length; i++) {
-  //       if (i < index) {
-  //         stars[i].classList.add('selected');
-  //       } else {
-  //         stars[i].classList.remove('selected');
-  //       }
-  //     }
-  //   }else{
-  //     const prevCardsStars = (cardNumber-1) * 5;
-
-  //     for (let i = prevCardsStars ; i < prevCardsStars+index; i++) {
-
-  //       if (i < prevCardsStars+index) {
-  //         stars[i].classList.add('selected');
-  //       } else {
-  //         stars[i].classList.remove('selected');
-  //       }
-  //     }
-
-  //   }
-
-
-  //   console.log(index)
-  //   return index
-  // }
-
-
-  
-
-
-
 
 
   onDropdownChange(bookDbValues: any, selectedValue:any) {
